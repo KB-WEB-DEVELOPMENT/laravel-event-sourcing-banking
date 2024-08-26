eaa3cf8152eb42f8d68f808ca840065671cbcf89
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-26 18:06:08 +0200

    Create OfferLoanReactor.php

---
+++ app/Domain/Transfer/Reactors/OfferLoanReactor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Domain\Transfer\Reactors;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Account;
+
+use App\Domain\Account\Events\OverdraftLimitReached;
+
+use App\Mail\LoanProposalMail;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Illuminate\Support\Facades\Mail;
+
+use Spatie\EventSourcing\EventHandlers\Reactors\Reactor;
+
+class OfferLoanReactor extends Reactor implements ShouldQueue
+{
+    public function __invoke(OverdraftLimitReached $event)
+    {
+        $user_id = Auth::id();
+		
+		$account = Account::where('user_id',$user_id)->first();
+
+        $user = $account->user;
+		
+		Mail::to($user->email)->send(new LoanProposalMail());
+    }
+}
